26bcef48a1f6af6e4c0fdacfd76092d69dbd3d16
Kristifor Milchev <kristifor@collaborativesoft.com>
2023-01-31 18:08:59 +0200

    Adding shape drawing to the chess board

--- lib/ui/views/game_board/game_board_viewmodel.dart
+++ lib/ui/views/game_board/game_board_viewmodel.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:chess/domain/models/board_square.dart';
+import 'package:chess_vectors_flutter/chess_vectors_flutter.dart';
+import 'package:draw_your_image/draw_your_image.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
@@ -8,16 +12,46 @@ class GameboardViewModel extends BaseViewModel {
   late Color _whiteColor = ThemeColors.cardBackground;
   late Color _blackColor = ThemeColors.mainThemeBackground;
 
+  late bool _drawing = false;
+  get canDraw => _drawing;
+  DrawController? _controller;
+  DrawController? get drawController => _controller;
+
   GameboardViewModel(Color? whiteSquares, Color? blackSquares) {
     if (whiteSquares != null) _whiteColor = whiteSquares;
     if (blackSquares != null) _blackColor = blackSquares;
+  }
+
+  ready() {
+    _squares = BoardSquare.getBoard(_blackColor, _whiteColor);
+    _squares.first.occupied = WhiteKing();
+    notifyListeners();
   }
 
   // ignore: prefer_final_fields
   List<BoardSquare> _squares = [];
   List<BoardSquare> get squares => _squares;
 
-  ready() {
-    _squares = BoardSquare.getBoard(_blackColor, _whiteColor);
+  void figureDropped(Object? current) {
+    var test = "";
+  }
+
+  dragStarted(String rowLabel) {
+    notifyListeners();
+  }
+
+  dragUpdated(DragUpdateDetails details) {
+    //  print(details.localPosition);
+  }
+
+  enabledDraw() {
+    _controller?.clear();
+    _drawing = true;
+    notifyListeners();
+  }
+
+  disableDraw() {
+    _drawing = false;
+    notifyListeners();
   }
 }
